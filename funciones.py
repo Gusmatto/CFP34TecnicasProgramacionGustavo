9f8cf682aea1bcb10c0e294f58b34fa12fb551cc
Gusmatto <gustavo.matto46@gmail.com>
2016-10-26 20:16:27 -0300

    Ingreso de bienvenida al usuario y pedido de elección del modo de juego.

--- funciones.py
+++ funciones.py
@@ -1,9 +1,1 @@
-def validarModoDeJuego(modo):
-    if modo == "A":
-        print()
-    elif modo == "P":
-        print()
-    elif modo == "S":
-        print()
-    else:
-        print("Elija una opcion valida")
+
