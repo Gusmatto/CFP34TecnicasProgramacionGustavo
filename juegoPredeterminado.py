258bba4694ef356d61f1f976e8c423d7f8fa5e0b
Gusmatto <gustavo.matto46@gmail.com>
2016-11-09 18:31:31 -0300

    Los cinco niveles de juego predeterminado.

---
+++ juegoPredeterminado.py
@@ -0,0 +1,26 @@
+
+nivelesPredeterminados = []
+
+def jugar():
+    print("Usted ha elegido predeterminado")
+    #inicializar los niveles predeterminados!
+
+def nivelesPredeterminados():
+    nivelesPredeterminados = 0
+
+
+nivelesPredeterminados1 = [["o", "o", ".", "o", "o"], [".", "o", ".", "o", "."], ["o", ".", ".", ".", "o"], ["o", "o", ".", "o", "o"], [".", ".", ".", "o", "o"]]
+nivelesPredeterminados2 = [[".", "o", ".", "o", "."], ["o", "o", ".", "o", "o"], [".", "o", ".", "o", "."], ["o", ".", "o", ".", "o"], ["o", ".", "o", ".", "o"]]
+nivelesPredeterminados3 = [["o", ".", ".", ".", "o"], ["o", "o", ".", "o", "o"], [".", ".", "o", ".", "."], ["o", ".", "o", ".", "."], ["o", ".", "o", "o", "."]]
+nivelesPredeterminados4 = [["o", "o", ".", "o", "o"], [".", ".", ".", ".", "."], ["o", "o", ".", "o", "o"], [".", ".", ".", ".", "o"], ["o", "o", ".", ".", "."]]
+nivelesPredeterminados5 = [[".", ".", ".", "o", "o"], [".", ".", ".", "o", "o"], [".", ".", ".", ".", "."], ["o", "o", ".", ".", "."], ["o", "o", ".", ".", "."]]
+
+print(nivelesPredeterminados1)
+print(nivelesPredeterminados2)
+print(nivelesPredeterminados3)
+print(nivelesPredeterminados4)
+print(nivelesPredeterminados5)
+
+
+
+
