e610adfa53829212179075f63ce56ec0259e23ed
Gusmatto <gustavo.matto46@gmail.com>
2016-11-09 18:47:56 -0300

    Los cinco niveles de juego predeterminado.

--- juegoPredeterminado.py
+++ juegoPredeterminado.py
@@ -4,22 +4,23 @@ nivelesPredeterminados = []
 def jugar():
     print("Usted ha elegido predeterminado")
     #inicializar los niveles predeterminados!
+    cargarNivelesPredeterminados()
+    print(nivelesPredeterminados)
 
-def nivelesPredeterminados():
-    nivelesPredeterminados = 0
+def cargarNivelesPredeterminados():
 
+    nivelesPredeterminados1 = [["o", "o", ".", "o", "o"], [".", "o", ".", "o", "."], ["o", ".", ".", ".", "o"], ["o", "o", ".", "o", "o"], [".", ".", ".", "o", "o"]]
+    nivelesPredeterminados2 = [[".", "o", ".", "o", "."], ["o", "o", ".", "o", "o"], [".", "o", ".", "o", "."], ["o", ".", "o", ".", "o"], ["o", ".", "o", ".", "o"]]
+    nivelesPredeterminados3 = [["o", ".", ".", ".", "o"], ["o", "o", ".", "o", "o"], [".", ".", "o", ".", "."], ["o", ".", "o", ".", "."], ["o", ".", "o", "o", "."]]
+    nivelesPredeterminados4 = [["o", "o", ".", "o", "o"], [".", ".", ".", ".", "."], ["o", "o", ".", "o", "o"], [".", ".", ".", ".", "o"], ["o", "o", ".", ".", "."]]
+    nivelesPredeterminados5 = [[".", ".", ".", "o", "o"], [".", ".", ".", "o", "o"], [".", ".", ".", ".", "."], ["o", "o", ".", ".", "."], ["o", "o", ".", ".", "."]]
 
-nivelesPredeterminados1 = [["o", "o", ".", "o", "o"], [".", "o", ".", "o", "."], ["o", ".", ".", ".", "o"], ["o", "o", ".", "o", "o"], [".", ".", ".", "o", "o"]]
-nivelesPredeterminados2 = [[".", "o", ".", "o", "."], ["o", "o", ".", "o", "o"], [".", "o", ".", "o", "."], ["o", ".", "o", ".", "o"], ["o", ".", "o", ".", "o"]]
-nivelesPredeterminados3 = [["o", ".", ".", ".", "o"], ["o", "o", ".", "o", "o"], [".", ".", "o", ".", "."], ["o", ".", "o", ".", "."], ["o", ".", "o", "o", "."]]
-nivelesPredeterminados4 = [["o", "o", ".", "o", "o"], [".", ".", ".", ".", "."], ["o", "o", ".", "o", "o"], [".", ".", ".", ".", "o"], ["o", "o", ".", ".", "."]]
-nivelesPredeterminados5 = [[".", ".", ".", "o", "o"], [".", ".", ".", "o", "o"], [".", ".", ".", ".", "."], ["o", "o", ".", ".", "."], ["o", "o", ".", ".", "."]]
+    nivelesPredeterminados.append(nivelesPredeterminados1)
+    nivelesPredeterminados.append(nivelesPredeterminados2)
+    nivelesPredeterminados.append(nivelesPredeterminados3)
+    nivelesPredeterminados.append(nivelesPredeterminados4)
+    nivelesPredeterminados.append(nivelesPredeterminados5)
 
-print(nivelesPredeterminados1)
-print(nivelesPredeterminados2)
-print(nivelesPredeterminados3)
-print(nivelesPredeterminados4)
-print(nivelesPredeterminados5)
 
 
 
