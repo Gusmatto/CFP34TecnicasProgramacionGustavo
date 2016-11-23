d412528dacee1d465b695b8961c4349b7a7f333a
Gusmatto <gustavo.matto46@gmail.com>
2016-11-23 20:07:39 -0300

    Se establecen los cambios de luces según la posición jugada.

--- juegoPredeterminado.py
+++ juegoPredeterminado.py
@@ -7,7 +7,7 @@ nivelesPredeterminados = []
 def jugar():
     print("Usted ha elegido predeterminado")
     cargarNivelesPredeterminados()
-    #TODO preguntar o conseguir o saber en qué nivel vamos a jugar ?
+
     tablero = get_tablero_para(1)
     interfaz_usuario.mostrar_tablero(tablero)
     print("")
@@ -29,10 +29,7 @@ def posicion_jugada(fila,columna,tablero):
     columnasPermitidas = ["A", "B", "C", "D", "E"]
 
 
-
-
     cambiar_luces(tablero,fila,columna)
-
 
 
 def cambiar_luces(tablero, fila, columna):
@@ -69,9 +66,6 @@ def cambiar_luces(tablero, fila, columna):
     interfaz_usuario.mostrar_tablero(tablero)
 
 
-
-"""TODO Sumar, restar a fila y a columna cuando corresponda"""
-
 def validar_coordenada():
 
     propuesta = input("Elija una coordenada(letra, número): ")
@@ -83,10 +77,10 @@ def validar_coordenada():
     return {"coordenadaValida": False, "valor": ""}
 
 def obtenerCoordenadas():
-    return {"A1": (0,0), "B1": (0,1), "C1": (0,2), "D1": (0,3), "E1": (0,4), "A2": (1,0), "B2": (1,1), "C2": (1,2),
-            "D2": (1,3), "E2": (1,4), "A3": (2,0), "B3": (2,1), "C3": (2,2), "D3": (2,3), "E3": (2,4), "A4": (3,0),
-            "B4": (3,1), "C4": (3,2), "D4": (3,3), "E4": (3,4), "A5": (4,0), "B5": (4,1), "C5": (4,2), "D5": (4,3),
-            "E5": (4,4)}
+    return {"A1": (0, 0), "B1": (0, 1), "C1": (0, 2), "D1": (0, 3), "E1": (0, 4), "A2": (1, 0), "B2": (1, 1), "C2": (1, 2),
+            "D2": (1, 3), "E2": (1, 4), "A3": (2, 0), "B3": (2, 1), "C3": (2, 2), "D3": (2, 3), "E3": (2, 4), "A4": (3, 0),
+            "B4": (3, 1), "C4": (3, 2), "D4": (3, 3), "E4": (3, 4), "A5": (4, 0), "B5": (4, 1), "C5": (4, 2), "D5": (4, 3),
+            "E5": (4, 4)}
 
 def cargarNivelesPredeterminados():
 
@@ -118,7 +112,7 @@ def get_tablero_para(nroNivel):
     if type(nroNivel) != int:
         return []
 
-    niveles_permitidos = (1,2,3,4,5)
+    niveles_permitidos = (1, 2, 3, 4, 5)
 
     if nroNivel not in niveles_permitidos:
         return []
